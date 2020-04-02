4054401f4d26fa91497ee836bf0d34b84903ce43
nquangminh96 <nquangminh9x@gmail.com>
2020-04-02 22:28:46 +0700

    [02/04/2020] youtube lib

--- app/src/main/java/com/android/tupple/robot/utils/constant/EntertainmentConstant.java
+++ app/src/main/java/com/android/tupple/robot/utils/constant/EntertainmentConstant.java
@@ -4,4 +4,5 @@ public class EntertainmentConstant {
     public static final String VIDEO_INTENT = "video_intent";
     public static final String AUDIO_INTENT = "audio";
     public static final String AUDIO_POSITION = "audio_position";
+    public static final String YOUTUBE_API_KEY = "AIzaSyBRrifbptZY6N5BcFXBYwv2-fTCgOnpwtc";
 }
